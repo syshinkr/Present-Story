ecdab8d495ebcace4a9aae15650b17c21beb3f60
syshinkr <syshinkr228@gmail.com>
2018-05-26 04:09:12 +0900

    오락실명 검색, 전체 목록 가져오기 구현

---
+++ app/src/main/java/com/project/admin/present_story/Activity/PeopleActivity.java
@@ -0,0 +1,15 @@
+package com.project.admin.present_story.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.project.admin.present_story.R;
+
+public class PeopleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_people);
+    }
+}
---
+++ app/src/main/java/com/project/admin/present_story/Adapter/MainViewPagerAdapter.java
@@ -0,0 +1,41 @@
+package com.project.admin.present_story.Adapter;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import com.project.admin.present_story.Model.FragmentModel;
+
+import java.util.ArrayList;
+
+public class MainViewPagerAdapter extends FragmentStatePagerAdapter {
+    ArrayList<FragmentModel> fragmentModels = new ArrayList<>();
+
+    public MainViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+    public void addFragment(int iconResId, String title, Fragment fragment) {
+        FragmentModel fragmentModel = new FragmentModel(iconResId, title, fragment);
+        fragmentModels.add(fragmentModel);
+    }
+
+    public FragmentModel getFragmentModel(int position) {
+        return fragmentModels.get(position);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return fragmentModels.get(position).getTitle();
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragmentModels.get(position).getFragment();
+    }
+
+    @Override
+    public int getCount() {
+        return fragmentModels.size();
+    }
+}
